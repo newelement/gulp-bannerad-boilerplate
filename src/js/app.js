574e82be56c185e471a1da5c1f8f1e5b22f10678
Don Jones <newelementdesigns@me.com>
2020-11-01 16:50:47 -0500

    Added js loop control and sample code

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,24 @@
+let $banner, 
+timer = 7500, // Adjust as needed
+loopCount = 1, // Adjust as needed
+intervalCount = 0, // Don't alter
+loopInterval = setInterval(animateLoop, (timer-100) ); // Don't alter
+
+function animateLoop(){
+
+if( loopCount === intervalCount ){
+	clearInterval(loopInterval);
+}
+
+$banner.classList.remove('animate');
+setTimeout(function(){
+	$banner.classList.add('animate');
+}, 100);
+
+intervalCount++;
+}
+
 document.addEventListener("DOMContentLoaded", function(){
-	// PUT STUFF IN HERE
+$banner = document.getElementById('banner-ad');
+animateLoop();
 });
